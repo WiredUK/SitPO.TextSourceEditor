efc6b2f30fca5cf0ca4dfe6d31ef9dbca10633e1
David Glass <dglass5@dxc.com>
2020-08-10 23:20:23 +0100

    Initial commit

---
+++ SitPO.TextSourceEditor.App/Configuration.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace SitPO.TextSourceEditor.App
+{
+    public class ConfigurationViewModel
+    {
+        public bool AutoSave { get; set; }
+        public List<TextSource> TextSources { get; set; }
+    }
+
+
+    public abstract class TextSource
+    {
+        public string Name { get; set; }
+    }
+
+    public class SimpleTextSource : TextSource
+    {
+        public string Value { get; set; }
+    }
+
+    public class NumericTextSource : TextSource
+    {
+        public int Value { get; set; }
+    }
+}
---
+++ SitPO.TextSourceEditor.App/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SitPO.TextSourceEditor.App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ConfigurationViewModel _viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Initialise();
+        }
+
+        private void Initialise()
+        {
+            _viewModel = new ConfigurationViewModel
+            {
+                AutoSave = true,
+                TextSources = new List<TextSource>
+                {
+                    new SimpleTextSource { Name = "Basic Text", Value = "Some text goes here" },
+                    new NumericTextSource { Name = "Team A Score", Value = 5 },
+                    new NumericTextSource { Name = "Team B Score", Value = 10 }
+                }
+            };
+
+            this.DataContext = _viewModel;
+        }
+
+        private void LoadConfigButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(_viewModel.AutoSave.ToString());
+        }
+    }
+}
